0bd4873411debbb99fe359379ef69a563b257a3a
NarooSister <150361471+NarooSister@users.noreply.github.com>
2023-12-06 09:48:27 +0900

    [level unrated] Title: 2의 영역, Time: 1.35 ms, Memory: 93.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,26 @@
+class Solution {
+ public int[] solution(int[] arr) {
+        int front = -1;
+        for (int i = 0; i <arr.length ; i++) {
+            if(arr[i]==2){
+                front = i;
+                break;
+            }
+        }
+      if(front == -1){
+            return new int[]{-1};
+        }
+        int back = 0;
+        for (int i = arr.length-1; i> -1; i--){
+            if(arr[i]==2){
+                back = i;
+                break;
+            }
+        }
+        int[] answer = new int[back - front + 1];
+        for (int i = front; i <back+1 ; i++) {
+           answer[i-front] = arr[i];
+        }
+        return answer;
+    }
+}
