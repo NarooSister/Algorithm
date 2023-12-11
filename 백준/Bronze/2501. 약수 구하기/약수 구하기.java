a78384bb31eb0b0ff98652b8b97dad0ff49cea2f
NarooSister <150361471+NarooSister@users.noreply.github.com>
2023-12-11 20:21:27 +0900

    [Bronze III] Title: 약수 구하기, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        int result = 0;
+
+        for (int i = 1; i <N+1 ; i++) {
+            if(N % i ==0){
+              count++;
+            }
+            if(K==count){
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
