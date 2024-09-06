5e432e8c6a0a9fe4e4f76cbaa73bdb13f0838d3f
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-06 10:19:04 +0900

    [Silver III] Title: 파일 정리, Time: 960 ms, Memory: 48636 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Map<String, Integer> file = new HashMap<>();
+        Map<String, Integer> file = new TreeMap<>();
 
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
@@ -15,18 +15,10 @@ public class Main {
             int idx = str.indexOf(".");
             String newStr = str.substring(idx+1);
             // map에 넣기, 중복인 경우 value++;
-            if(file.containsKey(newStr)){
-                file.put(newStr, file.get(newStr)+1);
-            } else {
-                file.put(newStr, 1);
-            }
+            file.put(newStr, file.getOrDefault(newStr, 0)+1);
         }
-        // 사전순으로 정렬하기 위해서 keySet으로 List에 넣는다
-        List<String> keySet = new ArrayList<>(file.keySet());
-        Collections.sort(keySet);
-        for (String key : keySet) {
-            Integer value = file.get(key);
-            System.out.println(key + " " + value);
+        for (Map.Entry<String, Integer> entry : file.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
