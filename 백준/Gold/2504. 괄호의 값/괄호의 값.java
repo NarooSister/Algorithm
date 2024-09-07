3aebc41195b02ec804903c054781d0825786afaa
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-07 16:36:35 +0900

    [Gold V] Title: 괄호의 값, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -16,6 +16,7 @@ public class Main {
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             switch (c) {
+                // 여는 괄호 시작할 때 tmp *2, *3 해주고 괄호 push
                 case '(':
                     stack.push(c);
                     tmp *= 2;
@@ -25,7 +26,16 @@ public class Main {
                     stack.push(c);
                     tmp *= 3;
                     break;
-
+                    
+                // 1. 닫는 괄호 나왔을 때 불가능한 상황
+                // 즉, stack이 비어있거나 최근 괄호가 여는 괄호가 아니라면
+                // success = false;
+                //
+                // 2. 불가능한 상황이 아니라면
+                // 2-1. 만약에 그 이전 괄호도 '(' 라면. -> '('가 두겹
+                // count에 tmp 더해줌
+                // 그 뒤에 여는 괄호 한 개 pop, tmp/2 /3
+                
                 case ')':
                     if (stack.isEmpty() || stack.peek() != '(') {
                         success = false;
@@ -36,7 +46,6 @@ public class Main {
                     }
                     stack.pop();
                     tmp /= 2;
-
                     break;
 
                 case ']':
@@ -49,7 +58,6 @@ public class Main {
                     }
                     stack.pop();
                     tmp /= 3;
-
                     break;
             }
         }
