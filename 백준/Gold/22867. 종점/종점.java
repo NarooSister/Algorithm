bda105160e2495832f91bb7b4d7ed3af415c1cde
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-11 21:50:53 +0900

    [Gold V] Title: 종점, Time: 852 ms, Memory: 58924 KB -BaekjoonHub

--- 백준/Gold/22867. 종점/종점.java
+++ 백준/Gold/22867. 종점/종점.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
 
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
