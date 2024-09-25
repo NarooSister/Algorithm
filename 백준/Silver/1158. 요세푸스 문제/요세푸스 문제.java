e5cfce52206ad3ff49cff62aa657a5a6f2a7f82e
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-25 15:19:11 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 208 ms, Memory: 16680 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,34 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+    // 원형 큐라고 생각하고 구현
+    // index가 큐의 사이즈를 넘어가면 나머지 연산으로 다시 구해줌
+    // index = (index+k-1) % queue.size();
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int i = 1; i < N+1; i++) {
-            queue.offer(i);
-        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         sb.append("<");
-       while(!queue.isEmpty()) {
-           for (int i = 0; i <K-1 ; i++) {
-             queue.offer(queue.poll());
-           }
-           if(queue.size()==1){
-               sb.append(queue.poll() + ">");
-           } else{
-               sb.append(queue.poll() + ", ");
-           }
-       }
+        for (int i = 0; i < n; i++) {
+            queue.offer(i+1);
+        }
+        int index = 0;
+
+        while (!queue.isEmpty()){
+            int count = 0;
+            index = (index+k-1) % queue.size();
+
+            for(int i : queue){
+                if(count == index){
+                    sb.append(i).append(", ");
+                    queue.remove(i);
+                    break;
+                }
+                count++;
+            }
+        }
+        sb.delete(sb.length()-2, sb.length()).append(">");
         System.out.println(sb);
     }
+
 }
