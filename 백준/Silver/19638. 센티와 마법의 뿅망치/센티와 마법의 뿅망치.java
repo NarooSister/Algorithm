2a65b4892c237adc7e374746217cb91efcc37831
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-26 10:46:15 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 352 ms, Memory: 32748 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -8,38 +8,37 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());   // 거인 인구 수
-        int h = Integer.parseInt(st.nextToken());   // 센티의 키
-        int t = Integer.parseInt(st.nextToken());   // 횟수 제한
-
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        
         PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> b-a);   // 우선순위 큐 최대힙
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int giantH = Integer.parseInt(st.nextToken());  // 각 거인의 키
+            int giantH = Integer.parseInt(st.nextToken());
             queue.offer(giantH);
         }
         
         int count = 0;
         while(count<t) {
             int maxGiant = queue.peek();
-            if (maxGiant < h) {    // 모든 거인이 센티보다 작을 때
-                System.out.println("YES");
-                System.out.println(count);
-                return; // 즉시 종료
+            if (maxGiant < h) {
+                System.out.println(sb.append("YES").append("\n").append(count));
+                return;
             }
-            if(maxGiant == 1) break;    // 1인 경우 생략
-            // 거인 뿅망치 적용
+            
+            if(maxGiant == 1) break;
             queue.poll();
             queue.offer(maxGiant/2);
             count++;
         }
-
+        
         if(queue.peek() >= h){
-            System.out.println("NO");
-            System.out.println(queue.peek());
+            System.out.println(sb.append("NO").append("\n").append(queue.peek()));
         } else{
-            System.out.println("YES");
-            System.out.println(count);
+            System.out.println(sb.append("YES").append("\n").append(count));
         }
     }
 }
