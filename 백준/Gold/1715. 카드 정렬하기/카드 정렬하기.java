7a51b4afbb733a79dd71e712b221fed05c266a05
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-26 15:35:17 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 312 ms, Memory: 25956 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,23 +1,6 @@
-
-
-/*
-1. 입력
-정수의 개수 N
-... N줄 동안
-숫자 묶음 각각의 크기
-
-2. 출력
-최소 비교 횟수
-
-3. 문제 접근법
-우선순위 큐
-
- */
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -26,11 +9,12 @@ public class Main {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         int n = Integer.parseInt(br.readLine());
         int result = 0;
+
         for (int i = 0; i < n; i++) {
-        queue.offer(Integer.parseInt(br.readLine()));
+            queue.offer(Integer.parseInt(br.readLine()));
         }
 
-        while(queue.size() > 1){
+        while (queue.size() > 1) {
             int first = queue.poll();
             int second = queue.poll();
             int sum = first + second;
