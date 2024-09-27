79e2a00d6fa9037dfadff42ca8e8fc4b1b5e294a
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-27 17:19:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 500 ms, Memory: 54616 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,7 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
 
 public class Main {
     static int n;
@@ -22,28 +26,31 @@ public class Main {
             int b = Integer.parseInt(st. nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
-            // 그래프 양쪽에 간선 추가
+            // 무방향이므로 그래프 양쪽에 간선 추가
             graph[a].add(new int[]{b,cost});
             graph[b].add(new int[]{a,cost});
         }
         System.out.println(prim());
     }
     static int prim(){
-        boolean[] visited = new boolean[n+1];
+        boolean[] visited = new boolean[n+1];   // 방문했던 정점
+        // 최소 가중치 노드가 먼저 나오도록 함
         PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        int totalCost = 0;
+        int totalCost = 0;  // 총 최소가중치의 합 = 최소비용
 
-        queue.offer(new int[]{1,0});
+        queue.offer(new int[]{1,0});   // 시작
         while(!queue.isEmpty()){
             int[] current = queue.poll();
-            int index = current[0];
-            int cost = current[1];
+            int index = current[0]; // 정점
+            int cost = current[1];  // 가중치
 
-            if(visited[index]) continue;
+            if(visited[index]) continue;    // 방문 했던 곳이면 넘기기
 
             visited[index] = true;
-            totalCost += cost;
-
+            totalCost += cost;  // 가중치 더해주기
+            
+            // 현재 정점에서 갈 수 있는 다른 정점으로의 간선 추가
+            // 그래프[index]로 돌리면서 간선 추가
             for(int[] edge : graph[index]){
                 if(!visited[edge[0]]){
                     queue.offer(new int[]{edge[0], edge[1]});
