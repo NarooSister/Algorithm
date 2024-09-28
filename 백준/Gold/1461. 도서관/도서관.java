829ebd4938fc016f696313247bbaa35ffc8cb8fd
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-28 17:17:46 +0900

    [Gold IV] Title: 도서관, Time: 96 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.java
+++ 백준/Gold/1461. 도서관/도서관.java
@@ -1,21 +1,11 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
-/*
-1. 입력
-책의 개수 N , 한번에 들 수 있는 책의 개수 M
-n개의 책의 위치
-
-2. 출력
-책을 모두 가져다 놓는 최소 걸음 수 출력
-
-3. 문제 접근법
-양수와 음수 나눠서 저장
-둘 다 내림차순으로 정렬하여 가장 먼 책부터 옮길 수 있도록 한다.
-마지막엔 돌아올 필요가 없으므로 가장 먼 거리를 마지막에 가야한다.
-결과에서 가장 먼 거리에서 돌아오는 걸음 수를 빼준다.
-
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,26 +25,31 @@ public class Main {
                 minus.add(-book);
             }
         }
+        
         Collections.sort(plus, Collections.reverseOrder()); // 내림차순
         Collections.sort(minus, Collections.reverseOrder());    // 내림차순
 
         int result = 0;
-        int maxDistance = 0;
+        // 한번에 두권씩 가져다둔다.
+        for (int i = 0; i < plus.size(); i+= m) {
+            // 두 권을 가져다 놓는 걸음 수 = 둘 중에 더 먼 책을 가져다 놓은 걸음 수
+            // 따라서 2를 곱해주면 된다.
+            result += plus.get(i)*2;    
+        }
+        for (int i = 0; i < minus.size(); i+= m) {
+            result += minus.get(i)*2;
+        }
 
+        int maxDistance = 0;
+        // 가장 먼 거리 구하기
         if(!plus.isEmpty()){
             maxDistance = plus.get(0);
         }
         if(!minus.isEmpty()){
             maxDistance = Math.max(maxDistance, minus.get(0));
         }
-
-        for (int i = 0; i < plus.size(); i+= m) {
-            result += plus.get(i)*2;
-        }
-        for (int i = 0; i < minus.size(); i+= m) {
-            result += minus.get(i)*2;
-        }
-        result -=maxDistance;
+        result -=maxDistance;   // 마지막에 가장 먼 거리를 빼준다.
+        
         System.out.println(result);
     }
 }
