e281b199f2a589f13e91e9da8acabac4ba12aa4b
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-09-30 15:03:15 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,5 +1,10 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 
 /*
 1. 입력
@@ -25,7 +30,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
