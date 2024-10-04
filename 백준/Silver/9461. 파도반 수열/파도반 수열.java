1e263cd1e928bc8e87a47a713e025b9b461802eb
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-10-04 18:20:40 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -5,7 +5,6 @@ import java.io.*;
 1. 입력
 테스트 케이스 t
 
-
 2. 출력
 각 테스트 케이스마다 파도반 수열 P(N) 구하기
 
@@ -17,22 +16,21 @@ public class Main {
     static long [] save = new long[101];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      
 
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            bw.write(padovan(n)+"\n");
+            System.out.println(padovan(n));
         }
-        bw.flush();
-        bw.close();
     }
+    
     static long padovan(int n){
         save[1] = 1;
         save[2] = 1;
         save[3] = 1;
         for (int i = 4; i <= n; i++) {
-            save[i] =  save[i-2]+save[i-3];
+            save[i] = save[i-2]+save[i-3];
         }
         return save[n];
     }
