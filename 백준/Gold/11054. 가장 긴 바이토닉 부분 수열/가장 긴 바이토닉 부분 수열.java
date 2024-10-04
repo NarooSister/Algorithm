937e15748ff6ec0681dcbeadd6d59f4b4b91f17e
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-10-04 14:37:29 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 128 ms, Memory: 15076 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,52 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
+/*
+1. 입력
+수열의 크기 n
+수열 A
+
+2. 출력
+가장 긴 바이토닉 수열의 길이
+
+3. 풀이
+가장 긴 바이토닉 부분 수열 구하기 :
+증가 수열 (0->n으로 탐색) + 감소수열 (n->0으로 탐색) -1 (자기 자신이 두번 들어가므로 1 빼줌)
+
+*/
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int [] arr = new int[n + 1];
-        int [] inDp = new int[n + 1];
-        int [] deDp = new int[n + 1];
+        int[] arr = new int[n];
+        int[] inArr = new int[n];
+        int[] deArr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        //증가수열
+        // 증가 수열 구하기
         for (int i = 0; i < n; i++) {
-            inDp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    inDp[i] = Math.max(inDp[i], inDp[j] + 1);
+            inArr[i] = 1;
+            for (int j = 0; j <i ; j++) {
+                if(arr[i] > arr[j]){
+                    inArr[i] = Math.max(inArr[i], inArr[j] +1);
                 }
             }
         }
 
-        //감소수열
-        for (int i = n - 1; i >= 0; i--) {
-            deDp[i] = 1;
-            for (int j = n - 1; j > i; j--) {
-                if (arr[i] > arr[j]) {
-                    deDp[i] = Math.max(deDp[i], deDp[j] + 1);
+        // 감소 수열 구하기
+        for (int i = n-1; i >= 0; i--) {
+            deArr[i] = 1;
+            for (int j = n-1; j > i; j--) {
+                if(arr[i] > arr[j]){
+                    deArr[i] = Math.max(deArr[i], deArr[j] +1);
                 }
             }
         }
 
         int max = 1;
-        //바이토닉 수열 = 증가수열 + 감소수열 - 1
         for (int i = 0; i < n; i++) {
-            if(inDp[i] + deDp[i] -1 > max) {
-                max = inDp[i] + deDp[i] - 1;
+            int sum = inArr[i] + deArr[i] -1;
+            if(sum > max){
+                max = sum;
             }
         }
-
         System.out.println(max);
     }
 }
