c043f5a2e23a067ac86ab0bde5136944f048cf47
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-10-05 13:45:45 +0900

    [level 1] Title: 폰켓몬, Time: 6.19 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+        int max = nums.length/2 ;
+        return Math.min(set.size(), max);
+    }
+}
