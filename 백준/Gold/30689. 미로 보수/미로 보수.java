86feda670d91d5a682e87e134ad0a5f769e22d88
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-11-10 00:59:52 +0900

    [Gold III] Title: 미로 보수, Time: 1128 ms, Memory: 248864 KB -BaekjoonHub

--- 백준/Gold/30689. 미로 보수/미로 보수.java
+++ 백준/Gold/30689. 미로 보수/미로 보수.java
@@ -1,76 +1,85 @@
+
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static char[][] directions;
-    static int[][] costs;
-    static boolean[][] visited; 
-    static boolean[][] done;
-    static int totalMinCost = 0;
-    static Map<Character, int[]> directionMap = Map.of(
-            'U', new int[]{-1, 0},
-            'D', new int[]{1, 0},
-            'L', new int[]{0, -1},
-            'R', new int[]{0, 1}
-    );
+	static int n, m;
+	static char[][] miro;
+	static int[][] miroCost;
+	static boolean[][] visited;
+	static boolean[][] done;
+	static int result = 0;
+	static Map<Character, int[]> dirMap = Map.of(
+		'U', new int[]{-1, 0},
+		'D', new int[]{1, 0},
+		'L', new int[]{0, -1},
+		'R', new int[]{0, 1}
+	);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
 
-        directions = new char[N][M];
-        costs = new int[N][M];
-        visited = new boolean[N][M];
-        done = new boolean[N][M];
+		miro = new char[n][m];
+		miroCost = new int[n][m];
+		visited = new boolean[n][m];
+		done = new boolean[n][m];
 
-        for (int i = 0; i < N; i++) {
-            directions[i] = sc.next().toCharArray();
-        }
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				miro[i][j] = str.charAt(j);
+			}
+		}
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                costs[i][j] = sc.nextInt();
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++) {
+				miroCost[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if(!done[i][j]){
+					dfs(i,j);
+				}
+			}
+		}
+		System.out.println(result);
+	}
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (!done[i][j]) {
-                    dfs(i, j);
-                }
-            }
-        }
+	static void dfs(int x, int y){
+		// 탈출 가능하면 리턴
+		if(x < 0 || x >= n || y < 0 || y >= m){
+			return;
+		}
 
-        System.out.println(totalMinCost);
-    }
+		int[] dir = dirMap.get(miro[x][y]);
+		int nx = x + dir[0];
+		int ny = y + dir[1];
 
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-        int[] dir = directionMap.get(directions[x][y]);
-        int nx = x + dir[0];
-        int ny = y + dir[1];
+		if(done[x][y]){
+			//사이클이 이미 완료된 경우 최소 비용 계산
+			int cost = miroCost[x][y];
+			int cx = x, cy = y;
+			do {
+				cost = Math.min(cost, miroCost[cx][cy]);
+				int[] nextDir = dirMap.get(miro[cx][cy]);
+				cx += nextDir[0];
+				cy += nextDir[1];
+			} while(!(cx == x && cy ==y));
+			result += cost;
+			return;
+		}
+	
+		if(visited[x][y]) return;
 
-        if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-            done[x][y] = true;
-        } else if (!done[nx][ny]) {
-            if (!visited[nx][ny]) {
-                dfs(nx, ny);
-            } else if (visited[nx][ny] && !done[nx][ny]) {
-                int cx = nx, cy = ny;
-                int minCost = Integer.MAX_VALUE;
-                do {
-                    minCost = Math.min(minCost, costs[cx][cy]);
-                    int[] nextDir = directionMap.get(directions[cx][cy]);
-                    cx += nextDir[0];
-                    cy += nextDir[1];
-                } while (cx != nx || cy != ny);
-                totalMinCost += minCost;
-            }
-
-            done[x][y] = true;
-        }
-
-        visited[x][y] = false;
-    }
+		visited[x][y] = true;
+		done[x][y] = true;
+		dfs(nx, ny);
+		done[x][y] = false;
+	}
 }
