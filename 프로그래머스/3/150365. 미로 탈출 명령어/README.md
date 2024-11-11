d7994e1f3b02bf9f5f8be3e60128ba8c2a3c3b4c
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-11-11 11:29:28 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.45 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 2.85 ms
+메모리: 73.2 MB, 시간: 1.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 11일 11:11:21
+2024년 11월 11일 11:29:26
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,45 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx = {1, 0, 0, -1};
-    static int[] dy = {0, -1, 1, 0};
-    static char[] directions = {'d', 'l', 'r', 'u'};
-
-    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {        
         StringBuilder sb = new StringBuilder();
         
         // 거리가 k일 때 탈출할 수 없는 경우(거리 불가능)
         int dist = Math.abs(x-r) + Math.abs(y-c);
+        
         if(dist > k || (k-dist) % 2 != 0){
             return "impossible";
         }
 
-        int startX = x, startY = y;
-        
-        for(int i = 0; i < k; i++){
-            boolean moved = false;
-            
-            for(int j = 0; j < 4; j++){
-                int nx = startX + dx[j];
-                int ny = startY + dy[j];
-                
-                if (nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
-                    int remainDist = Math.abs(nx - r) + Math.abs(ny - c);
-                    
-                    if(remainDist <= (k - i - 1)){
-                        sb.append(directions[j]);
-                        startX = nx;
-                        startY = ny;
-                        moved = true;
-                        break;
-                    }
-                }
+        while(k > 0){
+            // d : 아래로 이동 가능한 경우
+            if(x < n && k -1 >= Math.abs(x+1-r)+Math.abs(y-c)){
+                sb.append('d');
+                x++;
             }
-            if(!moved){
-                return "impossible";
+            // l : 왼쪽으로 이동 가능한 경우
+            else if(y > 1 && k-1 >= Math.abs(x-r)+Math.abs(y-1-c)){
+                sb.append('l');
+                y--;
             }
+            // r : 오른쪽으로 이동 가능한 경우
+            else if(y < m && k-1 >= Math.abs(x-r)+Math.abs(y+1-c)){
+                sb.append('r');
+                y++;
+            }
+            // u : 위쪽으로 이동 가능한 경우
+            else if(x > 1 && k-1 >= Math.abs(x-1-r)+Math.abs(y-c)){
+                sb.append('u');
+                x--;
+            }
+            // 마지막에 남은 이동 횟수를 감소
+            k--;
         }
+        
         
         return sb.toString();
     }
