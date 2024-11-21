12a3ef8e79b214a6afb671e014d098b806d2c05c
NarooSister <150361471+NarooSister@users.noreply.github.com>
2024-11-21 10:46:19 +0900

    [Gold II] Title: 저울, Time: 216 ms, Memory: 19876 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt(); 
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) {
+            weights[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(weights);
+
+        int target = 1;
+
+        for (int weight : weights) {
+            if (weight <= target) {
+                target += weight;
+            } else {
+                break;
+            }
+        }
+        System.out.println(target);
+    }
+}
