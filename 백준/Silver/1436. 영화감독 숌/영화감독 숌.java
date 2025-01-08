81cd431a3f645df64db2c3be7213e96d47fa4b6d
NarooSister <150361471+NarooSister@users.noreply.github.com>
2025-01-08 14:55:46 +0900

    [Silver V] Title: 영화감독 숌, Time: 352 ms, Memory: 88796 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+        int number = 666;
+        while (true) {
+            if (String.valueOf(number).contains("666")) {
+                count++;
+            }
+            if(count == n) break;
+            number++;
+        }
+        System.out.println(number);
+    }
+}
