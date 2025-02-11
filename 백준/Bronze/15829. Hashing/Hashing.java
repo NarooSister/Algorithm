6a13ece356a0c703f02332f16cc45bfaec47e763
NarooSister <150361471+NarooSister@users.noreply.github.com>
2025-02-11 14:38:52 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14280 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,15 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String str = sc.next();
-        char[] chars = str.toCharArray();
-        int hash = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int r = 31;
+        long m = 1234567891L;
+
+        long hash = 0;
+        long pow = 1;
+
         for (int i = 0; i < n; i++) {
-            hash += (chars[i] - 96) * Math.pow(31, i);
+            int value = str.charAt(i) -'a' + 1;
+            hash = (hash + (value * pow) % m) % m;
+            pow = (pow * r) % m;
         }
+
         System.out.println(hash);
     }
 }
